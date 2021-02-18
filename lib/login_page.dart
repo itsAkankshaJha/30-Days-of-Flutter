f635f2cdbd428ac6162fd75054bb178fd0f38129
Pranav Gupta <Pranav.19142@davietjal.org>
2021-02-18 23:53:00 +0530

    commit of day4

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -2,13 +2,26 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Center(
-        child: Text("Login",
-            style: TextStyle(
-            fontSize: 40.0,
-              fontWeight: FontWeight.bold,
-        ),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Image(
+              image: AssetImage('assets/images/login_image.png'),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "UserName",
+                labelText: "Username"
+              ),
+            ),
+            TextField(
+                decoration: InputDecoration(
+            hintText: "Password",
+            labelText: "Password"
+              ),
+            )
+          ]
         ),
       ),
     );
