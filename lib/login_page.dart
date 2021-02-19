b4dfe12ea6fdabaf59a5168995f39a48a25db9f9
Pranav Gupta <Pranav.19142@davietjal.org>
2021-02-19 23:48:34 +0530

    pending work of day-4 completed

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,27 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        reverse: false,
+        child: Center(
+          child: Column(children: [
+            SizedBox(
+              height: 150,
+              child: Center(
+                child: Text(
+                  "Welcome",
+                  style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.deepPurple,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
             Image(
               image: AssetImage('assets/images/login_image.png'),
             ),
-            TextField(
-              decoration: InputDecoration(
-                hintText: "UserName",
-                labelText: "Username"
+
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: "UserName",
+                  labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                  labelText: "Username",
+                ),
               ),
             ),
-            TextField(
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
                 decoration: InputDecoration(
-            hintText: "Password",
-            labelText: "Password"
+                  hintText: "UserName",
+                  labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                  labelText: "Username",
+                ),
               ),
-            )
-          ]
+            ),
+            SizedBox(
+              height: 18,
+            ),
+            ElevatedButton(onPressed: (){},
+                style: ButtonStyle(
+                ),
+                child: Container(
+                  margin: EdgeInsets.all(8),
+                  child: Text("Login",
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                ))
+          ]),
         ),
       ),
     );
