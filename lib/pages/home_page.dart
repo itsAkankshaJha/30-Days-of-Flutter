a30700a71ff8c77c640e6e20d02c030459442c18
Pranav Gupta <Pranav.19142@davietjal.org>
2021-03-01 18:16:03 +0530

    made json file

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,28 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:my_flutter/model/catalog.dart';
 import 'package:my_flutter/widgets/Items_Widgets.dart';
 import 'package:my_flutter/widgets/drawer.dart';
+import 'package:flutter/services.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+  loadData() async{
+   final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+   final jsonDecoded = jsonDecode(catalogJson);
+   var productsData = jsonDecoded["Products"];
+  }
   @override
   Widget build(BuildContext context) {
     var list = List<String>.generate(100, (index) => ModelItem.items[0].desc);
